5c9106474b79d8294f344d5e93f8eb3b9ca1feee
Kurt Minges <kurtminges@gmail.com>
2022-03-25 12:59:35 -0400

    Through 13.4.3

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,56 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Create the map object with a center and zoom level.
+// let map = L.map("mapid", {
+//     center: [
+//       40.7, -94.5
+//     ],
+//     zoom: 4
+//   });
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [34.0522, -118.2437],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "yellow",
+    weight: 5,
+    dashArray: 10,
+    opacity: .5
+}).addTo(map);
+
+// Get data from cities.js
+// let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+// cities.forEach(function(city) {
+//         console.log(city)
+//         L.circleMarker(city.location,{
+//             radius: city.population/100000,
+//             color: "orange",
+//             fillColor: '#ffffa1'
+//         })
+//         .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//     .addTo(map);
+// });
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    // id: 'mapbox/streets-v11', //other options: 'mapbox/outdoors-v11', 'mapbox/light-v10', 'mapbox/dark-v10', 'mapbox/satellite-v9', 'mapbox/satellite-streets-v11'
+    // tileSize: 512,
+    // zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
