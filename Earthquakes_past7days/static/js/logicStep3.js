a91bb8f36da1774f1cf883212d6d4a12e286ec86
Kurt Minges <kurtminges@gmail.com>
2022-03-27 14:36:47 -0400

    Through 13.6.4

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -11,38 +11,28 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 // We create the dark view tile layer that will be an option for our map.
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 18,
-  accessToken: API_KEY
+    maxZoom: 18,
+    accessToken: API_KEY
 });
-
-// Create the map object with center, zoom level and default layer.
-let map = L.map('mapid', {
-  center: [39.5, -98.5],
-  zoom: 3,
-  layers: [streets]
-})
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  "Streets": streets,
-  "Satellite Streets": satelliteStreets
-};
+    "Streets": streets,
+    "Satellite Streets": satelliteStreets
+  };
 
-// Create the earthquake layer for our map.
-let earthquakes = new L.layerGroup();
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+})
 
-// We define an object that contains the overlays.
-// This overlay will be visible all the time.
-let overlays = {
-  Earthquakes: earthquakes
-};
-
-// Then we add a control to the map that will allow the user to change
-// which layers are visible.
-L.control.layers(baseMaps, overlays).addTo(map);
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let earthquakesurl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+let earthquakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 // This function returns the style data for each of the earthquakes we plot on
 // the map. We pass the magnitude of the earthquake into a function
@@ -89,20 +79,17 @@ function getColor(magnitude) {
 }
 
 // Grabbing our GeoJSON data.
-d3.json(earthquakesurl).then(function(data) {
+d3.json(earthquakes).then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
-    // We turn each feature into a circleMarker on the map.
-    pointToLayer: function(feature, latlng) {
-          console.log(data);
-          return L.circleMarker(latlng);
-      },
-    style: styleInfo,
-      onEachFeature: function(feature, layer) {
-      layer.bindPopup("Location: " + feature.properties.place + "<br>Magnitude: " + feature.properties.mag);
-    }
-  }).addTo(earthquakes);
-
-    // Then we add the earthquake layer to our map.
-    earthquakes.addTo(map);
+  // We turn each feature into a circleMarker on the map.
+  pointToLayer: function(feature, latlng) {
+        console.log(data);
+        return L.circleMarker(latlng);
+    },
+  style: styleInfo,
+    onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h3> Location: " + feature.properties.place + "<h3> Magnitude: " + feature.properties.mag + "<h3/><h3/>");
+  }
+  }).addTo(map);
 });
