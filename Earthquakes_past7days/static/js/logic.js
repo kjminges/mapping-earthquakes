947a56449ea73bed2716e4d0112185631977e639
Kurt Minges <kurtminges@gmail.com>
2022-03-27 12:21:21 -0400

    Through 13.6.3

--- Earthquakes_past7days/static/js/logic.js
+++ Earthquakes_past7days/static/js/logic.js
@@ -41,7 +41,7 @@ function styleInfo(feature) {
   return {
     opacity: 1,
     fillOpacity: 1,
-    fillColor: "#ffae42",
+    fillColor: getColor(feature.properties.mag),
     color: "#000000",
     radius: getRadius(feature.properties.mag),
     stroke: true,
@@ -58,17 +58,38 @@ function getRadius(magnitude) {
   return magnitude * 4;
 }
 
+// This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+  if (magnitude > 5) {
+    return "#ea2c2c";
+  }
+  if (magnitude > 4) {
+    return "#ea822c";
+  }
+  if (magnitude > 3) {
+    return "#ee9c00";
+  }
+  if (magnitude > 2) {
+    return "#eecc00";
+  }
+  if (magnitude > 1) {
+    return "#d4ee00";
+  }
+  return "#98ee00";
+}
+
 // Grabbing our GeoJSON data.
 d3.json(earthquakes).then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
-
   // We turn each feature into a circleMarker on the map.
-  
   pointToLayer: function(feature, latlng) {
         console.log(data);
         return L.circleMarker(latlng);
     },
-  style: styleInfo
+  style: styleInfo,
+    onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h3> Location: " + feature.properties.place + "<br> <h4> Magnitude: " + feature.properties.mag + "<h4/><h3/>");
+  }
   }).addTo(map);
 });
--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -34,41 +34,20 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the airport GeoJSON URL
 let earthquakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-// This function returns the style data for each of the earthquakes we plot on
-// the map. We pass the magnitude of the earthquake into a function
-// to calculate the radius.
-function styleInfo(feature) {
-  return {
-    opacity: 1,
-    fillOpacity: 1,
-    fillColor: "#ffae42",
-    color: "#000000",
-    radius: getRadius(feature.properties.mag),
-    stroke: true,
-    weight: 0.5
-  };
-}
-
-// This function determines the radius of the earthquake marker based on its magnitude.
-// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
-function getRadius(magnitude) {
-  if (magnitude === 0) {
-    return 1;
-  }
-  return magnitude * 4;
+// Create a style for the lines.
+let myStyle = {
+  fillColor: "#ffffa1",
+  weight: 2,
+  color: "#0046f7"
 }
 
 // Grabbing our GeoJSON data.
 d3.json(earthquakes).then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
-
-  // We turn each feature into a circleMarker on the map.
-  
-  pointToLayer: function(feature, latlng) {
-        console.log(data);
-        return L.circleMarker(latlng);
-    },
-  style: styleInfo
+    style: myStyle,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<h3> Neighborhood (Area Code): <br> <h4>" + feature.properties.AREA_NAME + "<h4/><h3/>");
+    }
   }).addTo(map);
 });
